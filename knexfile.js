7073afb614033209fed4064166c89c09704f67e5
David Hernandez <David.Hernandez0118@gmail.com>
2016-09-23 12:52:03 -0600

    Finished Q#1

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,15 @@
 'use strict';
 
 module.exports = {
-  development: {},
-
-  test: {},
-
+  development: {
+    client: "pg",
+    connection:
+'postgres://localhost/bookshelf_dev'
+},
+  test: {
+    client: "pg",
+    connection:
+'postgres://localhost/bookshelf_test'
+},
   production: {}
 };
--- routes/books.js
+++ routes/books.js
@@ -7,4 +7,62 @@ const router = express.Router();
 
 // YOUR CODE HERE
 
+router.get('/', function(req, res) {
+  db.getAllPosts().then(posts => {
+    res.render('posts/posts', {
+      posts: posts,
+      title: 'An Excellent Blog'
+    })
+  })
+})
+
+router.get('/new', function(req, res) {
+  db.getAllUsers().then(users => {
+    res.render('posts/new', {
+      users: users,
+      title: 'An Excellent Blog'
+    })
+  })
+})
+
+router.get('/:id', function(req, res) {
+  db.getPost(req.params.id).then(post => {
+    res.render('posts/post', {
+      post: post,
+      title: 'An Excellent Blog'
+    })
+  })
+})
+
+router.post('/', function(req, res) {
+  db.createPost(req.body).then(() => {
+    res.redirect('/posts')
+  });
+});
+
+router.put('/:id', function(req, res) {
+  console.log(req.body)
+  db.updatePost(req.body).then(() => {
+    res.json({
+      'response': 'post updated'
+    })
+  });
+});
+
+router.delete('/:id', function(req, res) {
+  db.deletePost(req.params.id).then(() => {
+    res.json({
+      'response': 'post deleted'
+    })
+  });
+});
+
+router.get('/:id/edit', function(req, res) {
+  db.getPost(req.params.id).then(post => {
+    res.render('posts/edit', {
+      post: post,
+      title: 'An Excellent Blog'
+    })
+  });
+});
 module.exports = router;
